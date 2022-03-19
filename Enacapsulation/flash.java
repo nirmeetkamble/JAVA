ea56a497ec15116895fa9f13f2acbde7b4c5296f
nirmeetkamble <95981144+nirmeetkamble@users.noreply.github.com>
2022-03-20 01:35:12 +0530

    Add files via upload

---
+++ Enacapsulation/flash.java
@@ -0,0 +1,7 @@
+public class flash{
+    public static void main(String[] args){
+        person NewObj = new person();
+        NewObj.setName("Flash");
+        System.out.println(NewObj.getName());
+    }
+}
