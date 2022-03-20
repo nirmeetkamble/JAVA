5d7f781ec68feed3cbf1484e39e4ee40a8c28deb
nirmeetkamble <95981144+nirmeetkamble@users.noreply.github.com>
2022-03-21 04:11:14 +0530

    Add files via upload

---
+++ Arrays/list.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList; 
+
+public class list{
+    public static void main(String[] args){
+        ArrayList<String> Car = new ArrayList<String>();
+        Car.add("B.M.W");
+        Car.add("TATA");
+        Car.add("Volvo");
+        Car.add("Ford");
+        System.out.println(Car);
+    }
+}
