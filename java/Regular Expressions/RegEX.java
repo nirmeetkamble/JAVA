abe8a8e5349d1f8f448fe9593660e1465ad27b27
nirmeetkamble <95981144+nirmeetkamble@users.noreply.github.com>
2022-03-26 04:04:00 +0530

    Add files via upload

--- java/Regular Expressions/RegEX.java
+++ java/Regular Expressions/RegEX.java
@@ -2,8 +2,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class RegEX{
     public static void main(String[] args){
-       Pattern pattern = pattern.compile("hello" , Pattern.CASE_INSENSITIVE);// i dont understand what they have done in this line and below it
-       Matcher matcher = pattern.matcher("hello");
+       Pattern logic = logic.compile("hello" , Pattern.CASE_INSENSITIVE);
+       Matcher matcher = logic.matcher("hello");
        boolean matchfound = matcher.find();
        if(matchfound){
            System.out.println("Match found");
